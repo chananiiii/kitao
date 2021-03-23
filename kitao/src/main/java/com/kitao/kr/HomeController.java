16e3d83cb0ed03003e3354a6c6c2a19e2ee5691d
chananiiii <pchanwan@naver.com>
2021-03-24 01:54:48 +0900

    2021.03.24 글로벌프로퍼티 설정

--- kitao/src/main/java/com/kitao/kr/HomeController.java
+++ kitao/src/main/java/com/kitao/kr/HomeController.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
+	@Value("#{properties['ACTION_NAME']}")
+	private String ACTION;
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -25,7 +28,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+		logger.info("Test {}.", ACTION);
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
